88338628dd20676cab2c5e19d21f98da84d16825
jigspatel_it <jigspatel.04@gmail.com>
2020-01-13 14:50:12 +0530

    context api setup

---
+++ src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { ProductConsumer } from '../context';
+
+export default function HomePage() {
+  return (
+    <>
+      <ProductConsumer>
+        {value => {
+          console.log(value);
+          return <h1>hello from home page</h1>;
+        }}
+      </ProductConsumer>
+    </>
+  );
+}
