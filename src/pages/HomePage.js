096a60144daeb3befa4b4273b88aaffd341d4ce0
jigspatel_it <jigspatel.04@gmail.com>
2020-01-16 18:08:27 +0530

    home page services section

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
-import Hero from '../components/Hero'
-import {Link} from 'react-router-dom'
+import Hero from '../components/Hero';
+import { Link } from 'react-router-dom';
+
+import Services from '../components/HomePage/Services';
+import Featured from '../components/HomePage/Featured';
 
 export default function HomePage() {
   return (
     <>
-      <Hero title="awesome gadgets" max="true">
-        <Link to='/products' className='main-link' style={{margin:'2rem'}} >our products</Link>
+      <Hero title='awesome gadgets' max='true'>
+        <Link to='/products' className='main-link' style={{ margin: '2rem' }}>
+          our products
+        </Link>
       </Hero>
+      <Services />
+      <Featured />
     </>
   );
 }
